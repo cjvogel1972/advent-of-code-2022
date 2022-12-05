42dc3595c3338f7f05a2fa30a3876e54e206a96c
Chris Vogel <cjvogel1972@gmail.com>
2022-12-05 00:08:10 -0600

    Day 5 solution

--- day5/day5_test.go
+++ day5/day5_test.go
@@ -5,12 +5,22 @@ import (
 	"testing"
 )
 
-var lines = []string{}
+var lines = []string{
+	"    [D]    ",
+	"[N] [C]    ",
+	"[Z] [M] [P]",
+	" 1   2   3",
+	"",
+	"move 1 from 2 to 1",
+	"move 3 from 1 to 3",
+	"move 2 from 2 to 1",
+	"move 1 from 1 to 2",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, solvePart1(lines))
+	assert.Equal(t, "CMZ", solvePart1(lines))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(lines))
+	assert.Equal(t, "MCD", solvePart2(lines))
 }
