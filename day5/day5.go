2eaaa4f24417baab698e52abb13d7af3f5bef3ea
Chris Vogel <cjvogel1972@gmail.com>
2022-12-05 00:08:10 -0600

    Day 5 solution

--- day5/day5.go
+++ day5/day5.go
@@ -3,6 +3,7 @@ package day5
 import (
 	"advent-of-code-2022/utils"
 	"fmt"
+	"strings"
 )
 
 type Puzzle struct{}
@@ -11,14 +12,90 @@ type Puzzle struct{}
 func (Puzzle) Solve() {
 	lines := utils.ReadLines("day5/day5-input.txt")
 
-	fmt.Printf("Part 1: %d\n", solvePart1(lines))
-	fmt.Printf("Part 2: %d\n", solvePart2(lines))
+	fmt.Printf("Part 1: %s\n", solvePart1(lines))
+	fmt.Printf("Part 2: %s\n", solvePart2(lines))
 }
 
-func solvePart1(lines []string) int {
-	return 0
+func solvePart1(lines []string) string {
+	startCrateStackSize := 0
+	for i := 0; len(lines[i]) > 0; i++ {
+		startCrateStackSize++
+	}
+	startCrateStackSize--
+	numColumns := utils.ToInt(lines[startCrateStackSize][len(lines[startCrateStackSize])-1:])
+	crates := make([][]string, numColumns)
+	for i := 0; i < numColumns; i++ {
+		crates[i] = make([]string, 0)
+	}
+	for i := 0; i < startCrateStackSize; i++ {
+		for j := 0; j*4 < len(lines[i]); j++ {
+			crate := lines[i][j*4 : j*4+3]
+			if strings.ContainsRune(crate, '[') {
+				crates[j] = append(crates[j], string(crate[1]))
+			}
+		}
+	}
+
+	for i := startCrateStackSize + 2; i < len(lines); i++ {
+		var numCrates, column1, column2 int
+		_, err := fmt.Sscanf(lines[i], "move %d from %d to %d", &numCrates, &column1, &column2)
+		if err != nil {
+			panic(err)
+		}
+		column1--
+		column2--
+		top := make([]string, numCrates)
+		for j := 0; j < numCrates; j++ {
+			top[j] = crates[column1][numCrates-1-j]
+		}
+		crates[column2] = append(top, crates[column2]...)
+		crates[column1] = crates[column1][numCrates:]
+	}
+	topCrates := ""
+	for i := 0; i < len(crates); i++ {
+		topCrates = topCrates + crates[i][0]
+	}
+	return topCrates
 }
 
-func solvePart2(lines []string) int {
-	return 0
+func solvePart2(lines []string) string {
+	startCrateStackSize := 0
+	for i := 0; len(lines[i]) > 0; i++ {
+		startCrateStackSize++
+	}
+	startCrateStackSize--
+	numColumns := utils.ToInt(lines[startCrateStackSize][len(lines[startCrateStackSize])-1:])
+	crates := make([][]string, numColumns)
+	for i := 0; i < numColumns; i++ {
+		crates[i] = make([]string, 0)
+	}
+	for i := 0; i < startCrateStackSize; i++ {
+		for j := 0; j*4 < len(lines[i]); j++ {
+			crate := lines[i][j*4 : j*4+3]
+			if strings.ContainsRune(crate, '[') {
+				crates[j] = append(crates[j], string(crate[1]))
+			}
+		}
+	}
+
+	for i := startCrateStackSize + 2; i < len(lines); i++ {
+		var numCrates, column1, column2 int
+		_, err := fmt.Sscanf(lines[i], "move %d from %d to %d", &numCrates, &column1, &column2)
+		if err != nil {
+			panic(err)
+		}
+		column1--
+		column2--
+		top := make([]string, numCrates)
+		for j := 0; j < numCrates; j++ {
+			top[j] = crates[column1][j]
+		}
+		crates[column2] = append(top, crates[column2]...)
+		crates[column1] = crates[column1][numCrates:]
+	}
+	topCrates := ""
+	for i := 0; i < len(crates); i++ {
+		topCrates = topCrates + crates[i][0]
+	}
+	return topCrates
 }
