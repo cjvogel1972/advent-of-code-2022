f194afa4e20a8b89f3dc384356f6aea9addcdbb8
Chris Vogel <cjvogel1972@gmail.com>
2022-12-05 23:26:31 -0600

    Day 6 solution

--- day6/day6_test.go
+++ day6/day6_test.go
@@ -5,12 +5,26 @@ import (
 	"testing"
 )
 
-var lines = []string{}
+var lines = []string{
+	"mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+	"bvwbjplbgvbhsrlpgdmjqwftvncz",
+	"nppdvjthqldpwncqszvftbrmjlhg",
+	"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+	"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, solvePart1(lines))
+	assert.Equal(t, 7, solvePart1(lines[0]))
+	assert.Equal(t, 5, solvePart1(lines[1]))
+	assert.Equal(t, 6, solvePart1(lines[2]))
+	assert.Equal(t, 10, solvePart1(lines[3]))
+	assert.Equal(t, 11, solvePart1(lines[4]))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(lines))
+	assert.Equal(t, 19, solvePart2(lines[0]))
+	assert.Equal(t, 23, solvePart2(lines[1]))
+	assert.Equal(t, 23, solvePart2(lines[2]))
+	assert.Equal(t, 29, solvePart2(lines[3]))
+	assert.Equal(t, 26, solvePart2(lines[4]))
 }
