ac30f3881fb1e8bda70d4f36e33a4e8295172409
Chris Vogel <cjvogel1972@gmail.com>
2022-12-06 11:25:14 -0600

    Day 6 refactor duplicate code to util

--- utils/types.go
+++ utils/types.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"log"
 	"strconv"
+	"strings"
 )
 
 // ToInt converts a string to an integer, exiting the app if there is an error
@@ -14,6 +15,17 @@ func ToInt(s string) int {
 	return i
 }
 
+// InBetween checks to see if a number is between two given numbers
 func InBetween(i int, min int, max int) bool {
 	return min <= i && i <= max
+}
+
+// HasDuplicates checks if a string has duplicate characters
+func HasDuplicates(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if strings.Count(s, string(s[i])) != 1 {
+			return true
+		}
+	}
+	return false
 }
