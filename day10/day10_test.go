fbdf9a0c6b8615a0625ed41e2ffd8e900ed05d9e
Chris Vogel <cjvogel1972@gmail.com>
2022-12-10 12:25:32 -0600

    Day 10 solution

--- day10/day10_test.go
+++ day10/day10_test.go
@@ -1,16 +1,34 @@
 package day10
 
 import (
+	"advent-of-code-2022/utils"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var lines = []string{}
-
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, solvePart1(lines))
+	lines := utils.ReadLines("day10_test-input.txt")
+
+	assert.Equal(t, 13140, solvePart1(lines))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(lines))
+	lines := utils.ReadLines("day10_test-input.txt")
+
+	expected := []string{
+		"##..##..##..##..##..##..##..##..##..##..",
+		"###...###...###...###...###...###...###.",
+		"####....####....####....####....####....",
+		"#####.....#####.....#####.....#####.....",
+		"######......######......######......####",
+		"#######.......#######.......#######.....",
+	}
+	result := solvePart2(lines)
+	for i := 0; i < len(result); i++ {
+		name := fmt.Sprintf("line %d", i)
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, expected[i], result[i])
+		})
+	}
 }
