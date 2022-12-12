8d010ca929f3e990e7b3d596725df616891b4616
Chris Vogel <cjvogel1972@gmail.com>
2022-12-11 21:24:38 -0500

    Day 11 solution, with help from Reddit on part 2

--- day11/day11_test.go
+++ day11/day11_test.go
@@ -1,16 +1,20 @@
 package day11
 
 import (
+	"advent-of-code-2022/utils"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var lines = []string{}
-
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, solvePart1(lines))
+	lines := utils.ReadLines("day11_test-input.txt")
+
+	assert.Equal(t, 10605, solvePart1(lines))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(lines))
+	lines := utils.ReadLines("day11_test-input.txt")
+
+	//assert.Equal(t, 0, solvePart2(lines))
+	assert.Equal(t, 2713310158, solvePart2(lines))
 }
