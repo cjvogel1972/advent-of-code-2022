7c27048e30f0c42817bb0b17ca336b3b094281d7
Chris Vogel <cjvogel1972@gmail.com>
2024-12-08 17:18:03 -0600

    Solution for Day 16 Part 1

--- day16/day16.go
+++ day16/day16.go
@@ -3,6 +3,7 @@ package day16
 import (
 	"advent-of-code-2022/utils"
 	"fmt"
+	"strings"
 )
 
 type Puzzle struct{}
@@ -16,9 +17,232 @@ func (Puzzle) Solve() {
 }
 
 func solvePart1(lines []string) int {
-	return 0
+	valves := parseValves(lines)
+
+	totalPressure := dijkstra1(valves, "AA", 30)
+
+	return totalPressure
 }
 
 func solvePart2(lines []string) int {
-	return 0
+	valves := parseValves(lines)
+
+	totalPressure := dijkstra2(valves, "AA", 26)
+
+	return totalPressure
+}
+
+func parseValves(lines []string) map[string]valve {
+	valves := make(map[string]valve)
+	for _, l := range lines {
+		v := newValve(l)
+		valves[v.name] = v
+	}
+	return valves
+}
+
+func dijkstra1(valves map[string]valve, startValve string, maxTime int) int {
+	distance := make(map[string]int)
+	q := NewQueue()
+
+	q.enqueue(path{startValve, 0, make(map[string]bool), 0})
+
+	max := 0
+	for !q.empty() {
+		shortest := q.dequeue()
+		if shortest.time == maxTime-1 {
+			max = utils.Max(max, shortest.cost)
+			continue
+		}
+
+		adjacent := computeAdjacents(*shortest, valves)
+		for _, p := range adjacent {
+			d, ok := distance[p.hash()]
+			if !ok || p.cost > d {
+				distance[p.hash()] = p.cost
+				q.enqueue(p)
+			}
+		}
+	}
+
+	return max
+}
+
+func dijkstra2(valves map[string]valve, startValve string, maxTime int) int {
+	workingValvesCnt := 0
+	for _, v := range valves {
+		if v.rate > 0 {
+			workingValvesCnt++
+		}
+	}
+
+	distance := make(map[string]int)
+	double := make(map[string]int)
+	q := NewQueue()
+
+	q.enqueue(path{startValve, 0, make(map[string]bool), 0})
+
+	for !q.empty() {
+		shortest := q.dequeue()
+		if shortest.time == maxTime-1 {
+			pct := float32(len(shortest.openValves)) / float32(workingValvesCnt)
+			if pct > .25 && pct < .75 {
+				double[shortest.hashOpenValves()] = shortest.cost
+			}
+			continue
+		}
+
+		adjacent := computeAdjacents(*shortest, valves)
+		for _, p := range adjacent {
+			d, ok := distance[p.hash()]
+			if !ok || p.cost > d {
+				distance[p.hash()] = p.cost
+				q.enqueue(p)
+			}
+		}
+	}
+
+	max := 0
+	for a, aCost := range double {
+	inner:
+		for b, bCost := range double {
+			if a == b {
+				continue
+			}
+
+			for _, v := range strings.Split(a, " ") {
+				if strings.Contains(b, v) {
+					continue inner
+				}
+			}
+
+			max = utils.Max(max, aCost+bCost)
+		}
+	}
+
+	return max
+}
+
+func computeAdjacents(curr path, valves map[string]valve) []path {
+	result := make([]path, 0)
+	v := valves[curr.position]
+	if _, ok := curr.openValves[curr.position]; !ok && v.rate != 0 {
+		open := make(map[string]bool)
+		for s, _ := range curr.openValves {
+			open[s] = true
+		}
+		open[curr.position] = true
+		cost := curr.cost
+		for s, _ := range open {
+			cost += valves[s].rate
+		}
+		next := path{curr.position, curr.time + 1, open, cost}
+		result = append(result, next)
+	}
+
+	for _, t := range v.tunnels {
+		open := make(map[string]bool)
+		for s, _ := range curr.openValves {
+			open[s] = true
+		}
+		cost := curr.cost
+		for s, _ := range open {
+			cost += valves[s].rate
+		}
+		next := path{t, curr.time + 1, open, cost}
+		result = append(result, next)
+	}
+
+	return result
+}
+
+type valve struct {
+	name    string
+	rate    int
+	tunnels []string
+}
+
+func newValve(l string) valve {
+	var name string
+	var rate int
+	fmt.Sscanf(l, "Valve %s has flow rate=%d;", &name, &rate)
+
+	idx := strings.LastIndex(l, "valves")
+	var tunnels []string
+	if idx == -1 {
+		idx = strings.LastIndex(l, "valve")
+		t := l[idx+6:]
+		tunnels = []string{t}
+	} else {
+		t := l[idx+7:]
+		tunnels = strings.Split(t, ", ")
+	}
+
+	return valve{name, rate, tunnels}
+}
+
+type path struct {
+	position   string
+	time       int
+	openValves map[string]bool
+	cost       int
+}
+
+func (p path) hash() string {
+	return fmt.Sprintf("%s-%s-%d", p.position, p.hashOpenValves(), p.time)
+}
+
+func (p path) hashOpenValves() string {
+	open := make([]string, 0, len(p.openValves))
+	for s, _ := range p.openValves {
+		open = append(open, s)
+	}
+	s := fmt.Sprintf("%v", open)
+
+	return s[1 : len(s)-1]
+}
+
+type queue struct {
+	items []path
+}
+
+func NewQueue() queue {
+	items := make([]path, 0)
+	return queue{items}
+}
+
+func (q *queue) enqueue(p path) {
+	if len(q.items) == 0 {
+		q.items = append(q.items, p)
+		return
+	}
+	var insertFlag bool
+	for k, v := range q.items {
+		if p.cost > v.cost {
+			if k > 0 {
+				q.items = append(q.items[:k+1], q.items[k:]...)
+				q.items[k] = p
+				insertFlag = true
+			} else {
+				q.items = append([]path{p}, q.items...)
+				insertFlag = true
+			}
+		}
+		if insertFlag {
+			break
+		}
+	}
+	if !insertFlag {
+		q.items = append(q.items, p)
+	}
+}
+
+func (q *queue) dequeue() *path {
+	item := q.items[0]
+	q.items = q.items[1:len(q.items)]
+	return &item
+}
+
+func (q *queue) empty() bool {
+	return len(q.items) == 0
 }
--- day16/day16_test.go
+++ day16/day16_test.go
@@ -5,12 +5,23 @@ import (
 	"testing"
 )
 
-var lines = []string{}
+var lines = []string{
+	"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
+	"Valve BB has flow rate=13; tunnels lead to valves CC, AA",
+	"Valve CC has flow rate=2; tunnels lead to valves DD, BB",
+	"Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
+	"Valve EE has flow rate=3; tunnels lead to valves FF, DD",
+	"Valve FF has flow rate=0; tunnels lead to valves EE, GG",
+	"Valve GG has flow rate=0; tunnels lead to valves FF, HH",
+	"Valve HH has flow rate=22; tunnel leads to valve GG",
+	"Valve II has flow rate=0; tunnels lead to valves AA, JJ",
+	"Valve JJ has flow rate=21; tunnel leads to valve II",
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, solvePart1(lines))
+	assert.Equal(t, 1651, solvePart1(lines))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(lines))
+	assert.Equal(t, 1707, solvePart2(lines))
 }
